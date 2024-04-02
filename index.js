26e1681a0b7c4aa9a27ab545d51f93908e4a6171
Collins Ngetich <collinsngetichk@gmail.com>
2024-04-02 21:15:26 +0300

    modifies index.js

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Your code goes here
+document.addEventListener('DOMContentLoaded', function () {
+    // This function will be executed after the DOM content has loaded
+    const paragraph = document.getElementById('text');
+    if (paragraph) {
+        paragraph.textContent = "This is really cool!";
+    }
+});
